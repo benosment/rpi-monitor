8200cd1f27885bfd59165f8f8f14aa5125e35936
Ben Osment <benjamin.j.osment@gmail.com>
2017-01-08 09:33:36 -0500

    added basic date/time reporting

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    now = datetime.datetime.now()
+    time_string = now.strftime("%m-%d-%Y %H:%M:%S")
+    template_data = {'title': 'HELLO!',
+                     'time': time_string}
+    return render_template('main.html', **template_data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
